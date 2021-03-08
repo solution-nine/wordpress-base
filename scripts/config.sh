dfa88be7b2e972b57c1a1bc90dd80e62422ae690
Felipe Oduardo Sierra <felipe@solutionnine.com>
2021-03-08 13:20:00 -0700

    Updates the base group_vars

--- scripts/config.sh
+++ scripts/config.sh
@@ -10,5 +10,10 @@ TLD=${site[1]}
 
 echo '[Configuring Site: ${1}]'
 sed -i '' "s/example.com/${DOMAIN}.${TLD}/g" package.json
+find "trellis-config" -type f -print0 | xargs -0 sed -i '' "s/example.test/${DOMAIN}.test/g"
 find "trellis-config" -type f -print0 | xargs -0 sed -i '' "s/example.com/${DOMAIN}.${TLD}/g"
 find "trellis-config" -type f -print0 | xargs -0 sed -i '' "s/example_com/${DOMAIN}_${TLD}/g"
+echo '[Setup Vault Password]'
+openssl rand -base64 32 >> trellis-config/.vault_pass
+echo '[TODO: Save this password]'
+echo $(<trellis-config/.vault_pass)
